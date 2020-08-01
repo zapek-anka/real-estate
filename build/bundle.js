c512388ddcc2643ae12d2a2598ef8a9bc518060c
shepelina <schepelina.an@yandex.ru>
2020-08-01 23:23:28 +0400

    js added

--- build/bundle.js
+++ build/bundle.js
@@ -101,7 +101,66 @@ __webpack_require__.r(__webpack_exports__);
 /* 1 */
 /***/ (function(module, exports) {
 
-console.log('congrats!');
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var RealEstate = /*#__PURE__*/function () {
+  function RealEstate() {
+    _classCallCheck(this, RealEstate);
+
+    this.showMore = document.querySelector('.js-show-more');
+    this.scrollTop = document.querySelector('.js-go-up');
+    this.hiddenItems = _toConsumableArray(document.querySelectorAll('.js-hidden'));
+    this.showMore.addEventListener('click', this.showHidden.bind(this));
+    this.scrollTop.addEventListener('click', this.goUp.bind(this));
+    this.anchors = _toConsumableArray(document.querySelectorAll('a[href^="#"]'));
+    this.anchors.forEach(function (anchor) {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  }
+
+  _createClass(RealEstate, [{
+    key: "showHidden",
+    value: function showHidden() {
+      this.hiddenItems.forEach(function (item) {
+        item.classList.remove('m-hidden');
+      });
+      this.showMore.classList.add('m-hidden');
+    }
+  }, {
+    key: "goUp",
+    value: function goUp() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+  }]);
+
+  return RealEstate;
+}();
+
+new RealEstate();
 
 /***/ }),
 /* 2 */
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
-console.log('congrats!');
+class RealEstate {
+    constructor() {
+        this.showMore = document.querySelector('.js-show-more');
+        this.scrollTop = document.querySelector('.js-go-up');
+        this.hiddenItems = [...document.querySelectorAll('.js-hidden')];
+        this.showMore.addEventListener('click', this.showHidden.bind(this));
+        this.scrollTop.addEventListener('click', this.goUp.bind(this));
+        this.anchors = [...document.querySelectorAll('a[href^="#"]')];
+        this.anchors.forEach((anchor) => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    }
+
+    showHidden() {
+        this.hiddenItems.forEach((item) => {
+            item.classList.remove('m-hidden');
+        });
+        this.showMore.classList.add('m-hidden');
+    }
+
+    goUp() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+}
+
+new RealEstate();
